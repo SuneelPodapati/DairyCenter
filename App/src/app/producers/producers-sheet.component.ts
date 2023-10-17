0ea8a12bfe27a333c3bd8ac1d182041007c38ef5
Suneel Podapati <podapati.suneel@gmail.com>
2023-10-18 05:28:44 +0800

    FIx special rate regex

--- App/src/app/producers/producers-sheet.component.ts
+++ App/src/app/producers/producers-sheet.component.ts
@@ -78,7 +78,7 @@ export class ProducersSheetComponent implements OnInit {
     columns: [
       { title: 'Producer Code', data: 'code', validator: (value, cb) => cb(value > 0 && this.producerIds.filter(x => x == value).length <= 1) },
       { title: 'Name', data: 'name', validator: /^[a-zA-Z0-9.\s,-]{3,}/, renderer: this.nameRenderer },
-      { title: 'Special Rate', data: 'specialRate', validator: /(^\d*\.?\d+-\d*\.?\d+:\d*\.?\d+)(,\s+(\d*\.?\d+-\d*\.?\d+:\d*\.?\d+))*$/ }, // 1.1-2.2:3.3
+      { title: 'Special Rate', data: 'specialRate', validator: /^$|(^\d*\.?\d+-\d*\.?\d+:\d*\.?\d+)(,\s+(\d*\.?\d+-\d*\.?\d+:\d*\.?\d+))*$/ }, // 1.1-2.2:3.3
       { title: 'Contact No.', data: 'contactNumber', validator: /^$|^\d{10}$/ },
       { title: 'Bank Account No.', data: 'bankAccountNumber', validator: /^$|^[0-9]{5,20}$/ },
       { title: 'Bank IFSC Code', data: 'bankIfscCode', validator: /^$|^[A-Za-z]{4}\d{7}$/ },
